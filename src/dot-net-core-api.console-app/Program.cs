fbc54d164e29a2c0f22c7dc43ba728da971054e5
Andy Watt <andy.watt83@gmail.com>
2019-09-14 18:54:47 +0100

    #1 - Some inital setup

---
+++ src/dot-net-core-api.console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_core_api.console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
