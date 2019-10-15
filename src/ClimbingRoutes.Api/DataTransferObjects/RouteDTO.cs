c0510e1abdbce864bb28d785f39aeef8f2d922d9
Andy Watt <andy.watt83@gmail.com>
2019-10-15 21:19:17 +0100

    starting to think about testing the database

--- src/ClimbingRoutes.Api/DataTransferObjects/RouteDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace ClimbingRoutes.Api.DataTransferObjects
-{
-    public class RouteDTO
-    {
-        public string Name { get; set; }
-    }
-
-}
---
+++ test/ClimbingRoutes.Database.Test/TestClimbingRoutesContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ClimbingRoutes.Database.Test
+{
+    public class TestClimbingRoutesContext
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
